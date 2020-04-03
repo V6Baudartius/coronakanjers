847d96b32d4fe9460a118cfc2eb4c54b7e251e92
SytzeHorjus <sytzehorjus@gmail.com>
2020-04-03 08:47:25 +0200

    Update Ragnarok.py

--- Project/game_versies/Ragnarok.py
+++ Project/game_versies/Ragnarok.py
@@ -2,8 +2,9 @@
   #pygame is nodig, duh
   #os wordt gebruikt bij het inladen van images
   #math wordt om keersommen te kunnen doen
-  #sys weet ik ook niet, maar kan nog handig zijn 
-import pygame, os, math, sys
+  #sys weet ik ook niet, maar kan nog handig zijn
+  #time wordt gebruikt bij het maken van tijdsgerelateerde scripts
+import pygame, os, math, sys, time
 
 
 
@@ -387,15 +388,87 @@ class levelparent():
                         stenen.append(steen(x*gridsize, y*gridsize))
                     elif current ==(0, 0, 255, 255):
                         dozen.append( doos(x*gridsize, y*gridsize))
-                    
+#EINDE LEVELTEST                    
+#BEGIN STOPWATCH
 
+#Dit slaat de tijd op op het moment dat dit script wordt gestart.
+#time.gmtime() geeft een aray aan variabelen(?). Waarbij [3] de uren geeft, [4] de minuten en [5] de seconden.
+start = time.gmtime()
+
+#hier begint de customclock class. En dan kan je denken:"Sytze, waarom noem je class niet gwn clock?". NOU OMDAT PYGAME AL EEN OBJECT HEEFT GENAAMD CLOCK!!!!!!!!!!!    
+class stopwatch():
+    def __init__(self, kleurrect, kleurtijd, xleft, ytop, uren, outline, dikte, kleuroutline, start):
+        self.kleurrect = kleurrect
+        self.kleurtijd = kleurtijd
+        self.xleft = xleft
+        self.ytop = ytop
+        self.uren = uren
+        self.outline = outline
+        self.dikte = dikte
+        self.kleuroutline = kleuroutline
+        self.start = start
+#Hier begint de functie drawclock waarin een stopwatch wordt geïnitïeerd en daarna ook getekend/
+    def drawstopwatch(self):
+        #Door global screen te doen haal kan je screen gebruiken die eerder gedefnieerd is.
+        #Dit komt doordat screen zich in de globale sector bevindt en deze class zit in de lokale sector.
+        global screen
+        #Slaat de tijd op van nu. Dit is dus de steeds veranderende tijd.
+        now = time.gmtime()
+        #Hier wordt het verschil in seconden berekent tussen de starttijd en de tijd van nu.
+        seconde = now[5]-self.start[5]
+        #Hier wordt het verschil in minuten berekent tussen de starttijd en de tijd van nu.
+        minuut = now[4]-self.start[4]
+        #Hier wordt het verschil in uren berekent tussen de starttijd en de tijd van nu.
+        uur = now[3]-self.start[3]
+
+        #De lettertype grootte
+        grootte = 25
+
+        #De font (lettertype) wordt  hier bepaald.
+        default_font = pygame.font.Font("C:/Windows/Fonts/comicz.TTF", grootte)
+        #Als het verschil in seconden negatief wordt, wordt het met behulp van dit trucje weer goed positief gemaakt.
+        if seconde < 0:
+            seconde += 60
+            minuut -= 1
+            
+        #Als het verschil in minuten negatief wordt, wordt het met behulp van dit trucje weer goed positief gemaakt.
+        if minuut < 0:
+            minuut += 60
+            uur -= 1
+            
+        #Het rechthoekje met tijd erin kan verschillen door het hebben van uren in de tijd of niet. De breedte van het rechthoekje wordt hier dan gecorigeerd.
+        if self.uren:
+            breedte = 170    
+        if not self.uren:
+            breedte = 105
+            
+        #De hoogte blijft wel altijd gelijk.    
+        hoogte = 30    
+
+        #Als self.outline is True dan wordt er eerst een grotere rechthoek getekent waarna het kleinere het rechthoek er overheen wordt getekent waardoor het lijkt
+        #alsof er een outline is getekent om het kleinere rechthoek heen.
+        if self.outline and self.dikte>0 and self.dikte<10:
+            #grotere rechthoek wordt getekent
+            pygame.draw.rect(screen, self.kleuroutline, (self.xleft-5-self.dikte, self.ytop+5-self.dikte, breedte+2*self.dikte, hoogte+2*self.dikte))
+        #kleinere rechthoek wordt getekent    
+        pygame.draw.rect(screen, self.kleurrect, (self.xleft-5, self.ytop+5, breedte, hoogte))
+        #als self.uren false is dan worden de uren niet in het tijdsblokje gegeven. Anders wel.
+        if not self.uren:
+            #tekst zonder uren
+            tijd_tekst = default_font.render(str(minuut).zfill(2) + ' : ' + str(seconde).zfill(2), True, self.kleurtijd)
+        if self.uren:
+            #tekst met uren
+            tijd_tekst = default_font.render(str(uur).zfill(2) + ' : ' + str(minuut).zfill(2) + ' : ' + str(seconde).zfill(2), True, self.kleurtijd)
+        #tekst wordt getekent
+        screen.blit(tijd_tekst, (self.xleft,self.ytop))
+
+#EINDE STOPWATCH
 
 creatie = levelparent()
-
 enemy1 = enemy_lopend(2000,400)
 michiel = monster(-420, 69, 0.1)
-
-#EINDE LEVELTEST
+#De stopwatch wordt hier geinitialiseerd
+klok1 = stopwatch((0,255,0),(255,255,255),820,5,True,True,5,(0,0,0),start)
 
 
 print(allCollisionObjects)
@@ -435,7 +508,7 @@ while True:
         ragnar.sprite = imgload('ragnar_dood.png')
         spriteveranderalsdood = False
 
-        
+    klok1.drawstopwatch()  #de klok wordt hier getekent  
     ragnar.drawupdate()
     enemy1.drawupdate()    
     
